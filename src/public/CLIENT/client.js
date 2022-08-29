b58464cd88bc72a30422383fd526aa64e8901a49
Antoine Duval <antoine@cuffel.fr>
2022-08-29 13:00:13 +0200

    Update

--- src/public/CLIENT/client.js
+++ src/public/CLIENT/client.js
@@ -21,7 +21,7 @@ class Client {
                 if(!chromiumIsLaunched){
                   SELF.Main.Log("Chromium is off.");
                   SELF.Main.Log("We start Chromium (http://localhost:" + SELF.Main.Settings.WebServerPort + ")");
-                  SELF.Terminal("chromium-browser http://localhost:" + SELF.Main.Settings.WebServerPort + " --no-sandbox --autoplay-policy=no-user-gesture-required", "", function (_error_code, _messages) {
+                  SELF.Terminal("chromium-browser http://localhost:" + SELF.Main.Settings.WebServerPort + " --autoplay-policy=no-user-gesture-required", "", function (_error_code, _messages) {
                       if (_error_code === 0) {
                         SELF.Main.Log("We started Chromium.");
                       } else {
